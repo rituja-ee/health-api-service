85b0a7a6f2e066ca97ac914b8feadd54c95b0f53
Rituja Chaudhari <ritujachaudhari@Ritujas-MacBook-Pro.local>
2025-10-03 17:30:41 +0530

    Add apis to get system metrics and health status

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# devops-upskilling
-Contains assignments from DevOps Upskilling Program
+# Health Service
+Give basic system metrics and health status
+
+# Pre-requisite
+- Python - 3.11.9
+- Poetry - 2.2.1
+
+# Dependencies
+- Fast API
+- uvicorn
+- psutil
+
+# How to run
+`poetry run uvicorn app.main:app --reload --host 127.0.0.1 --port 8000`
--- app/main.py
+++ app/main.py
@@ -1,7 +1,64 @@
 from fastapi import FastAPI
+import psutil
 
 app = FastAPI()
 
-@app.get("/health")
+@app.get("/")
 def health_check():
-    return {"status": "Hello World"}
+    return {"Health API service is running"}
+
+@app.get("/metrics")
+def resource_metrics():
+    load_averages = psutil.getloadavg()  
+    cpu_count = psutil.cpu_count() or 1
+    load_percentages = [x / cpu_count * 100 for x in load_averages]
+
+    return {
+        "cpu": {
+            "percent": psutil.cpu_percent(interval=1),
+            "cores": psutil.cpu_count(logical=True),
+            "load_avg": load_averages,
+            "load_avg_percent": load_percentages,
+        },
+        "memory": {
+            "total": psutil.virtual_memory().total,
+            "available": psutil.virtual_memory().available,
+            "percent": psutil.virtual_memory().percent,
+        },
+        "disk": {
+            "total": psutil.disk_usage("/").total,
+            "used": psutil.disk_usage("/").used,
+            "free": psutil.disk_usage("/").free,
+            "percent": psutil.disk_usage("/").percent,
+        }
+    }
+
+@app.get("/health")
+def resource_health_check():
+    status = "healthy"
+    errors = []
+    CPU_THRESHOLD = 80
+    MEMORY_THRESHOLD = 80
+    DISK_THRESHOLD = 90
+
+    memory = psutil.virtual_memory()
+    cpu_percent = psutil.cpu_percent(interval=1)
+    disk = psutil.disk_usage('/')
+
+    if cpu_percent > CPU_THRESHOLD:
+        status = "unhealthy"
+        errors.append(f"High CPU utilization: {cpu_percent}%")
+    if memory.percent > MEMORY_THRESHOLD:
+        status = "unhealthy"
+        errors.append(f"High memory utilization: {memory.percent}")
+    if disk.percent > DISK_THRESHOLD:
+        status = "unhealthy"
+        errors.append(f"High disk utilization: {disk.percent}%")
+    
+    return {
+        "status": status,
+        "cpu_percent": cpu_percent,
+        "memory_percent": memory.percent,
+        "disk_percent": disk.percent,
+        "errors": errors,
+    }
